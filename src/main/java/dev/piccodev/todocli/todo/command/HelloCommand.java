8d124ebc69d03c269576a7adacdd9c5af6523f50
Rafiere <rafapgbr@gmail.com>
2023-11-05 23:53:40 -0300

    feat: add hello command with exceptions and availability.

---
+++ src/main/java/dev/piccodev/todocli/todo/command/HelloCommand.java
@@ -0,0 +1,45 @@
+package dev.piccodev.todocli.todo.command;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.Availability;
+import org.springframework.shell.AvailabilityProvider;
+import org.springframework.shell.command.CommandHandlingResult;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.CommandAvailability;
+import org.springframework.shell.command.annotation.ExceptionResolver;
+
+import java.time.LocalDate;
+
+/* Tudo o que ficar no "@Command" externo será herdado pelos "@Command" internos, a não ser que ele seja sobrescrito.*/
+@Command(group = "Introduction")
+public class HelloCommand {
+
+    /* Esse é um comando que pode ser chamado pela CLI através do comando "hello-world". */
+    @Command(command = "hello-world", description = "Prints 'Hello World'.")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    /* Esse comando estará disponível ou não para ser chamado de acordo com o "oddDayCheck", que é um bean da aplicação e
+    * faz essa verificação.*/
+    @Command(command = "hello-world-with-availability", description = "Prints 'Hello World'. The method will work if we are on an odd number day.")
+    @CommandAvailability(provider = "oddDayCheck")
+    public String helloWorldIfHasAvailability(){
+        return "Hello World with availability!";
+    }
+
+    @Command(command = "hello-world-with-runtime-exception", description = "Prints 'Hello World' and throw a runtime exception.")
+    public String throwRuntimeException(){
+        System.out.println("Hello World!");
+
+        throw new RuntimeException();
+    }
+
+//    O método abaixo só funcionará dentro dessa classe, e não como um handler global, por isso, deveremos utilizar outra
+    //classe
+//    @ExceptionResolver
+//    public CommandHandlingResult resolve(Exception e) {
+//        System.out.println("Entrou!");
+//        return CommandHandlingResult.of("Exception", 42);
+//    }
+}
