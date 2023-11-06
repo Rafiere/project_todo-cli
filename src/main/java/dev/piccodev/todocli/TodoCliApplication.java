b72b6754d278a00c7ea64639b6411f2a037983b5
Rafiere <rafapgbr@gmail.com>
2023-11-05 23:52:50 -0300

    feat: add @CommandScan annotation

--- src/main/java/dev/piccodev/todocli/TodoCliApplication.java
+++ src/main/java/dev/piccodev/todocli/TodoCliApplication.java
@@ -2,8 +2,11 @@ package dev.piccodev.todocli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@CommandScan
 public class TodoCliApplication {
 
 	public static void main(String[] args) {
